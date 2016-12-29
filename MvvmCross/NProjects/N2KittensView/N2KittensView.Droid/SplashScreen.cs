a3a1e2c467da22db162b55fd0a78ec33fedccf5c
NeuUser <divikiran.ravela@neudesic.com>
2016-12-29 16:29:03 +0530

    Android Setup
    Android Setup

---
+++ MvvmCross/NProjects/N2KittensView/N2KittensView.Droid/SplashScreen.cs
@@ -0,0 +1,21 @@
+using Android.App;
+using Android.Content.PM;
+using MvvmCross.Droid.Views;
+
+namespace N2KittensView.Droid
+{
+    [Activity(
+        Label = "N2KittensView.Droid"
+        , MainLauncher = true
+        , Icon = "@mipmap/icon"
+        , Theme = "@style/Theme.Splash"
+        , NoHistory = true
+        , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenActivity
+    {
+        public SplashScreen()
+            : base(Resource.Layout.SplashScreen)
+        {
+        }
+    }
+}
