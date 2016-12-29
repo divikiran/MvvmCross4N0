915aa70f44a63d1405b86b223d91e65280059a3a
NeuUser <divikiran.ravela@neudesic.com>
2016-12-29 15:05:55 +0530

    Final TipCalc
    Final commit to TipCalc

--- MvvmCross/NProjects/N1TipCalc/TicCalc.Touch/Views/FirstView.cs
+++ MvvmCross/NProjects/N1TipCalc/TicCalc.Touch/Views/FirstView.cs
@@ -1,6 +1,8 @@
 using System.Drawing;
+using CoreGraphics;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.iOS.Views;
+using N1TipCalc.ViewModels;
 using ObjCRuntime;
 using UIKit;
 
@@ -19,11 +21,55 @@ namespace TicCalc.Touch.Views
             base.ViewDidLoad();
 
 			// ios7 layout
-			if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
-				EdgesForExtendedLayout = UIRectEdge.None;
+			//if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+			this.EdgesForExtendedLayout = UIRectEdge.None;
 
-			var label1 = new UILabel(new RectangleF(0, 0, 300, 40)) { Text = "Test", BackgroundColor = UIColor.Green };
-			Add(label1);
+			var subTotalLabel = new UILabel(new RectangleF(10, 0, 300, 40)) { Text = "SubTotal" };
+			Add(subTotalLabel);
+
+			var subTotalText = new UITextField(new CGRect(10, 40, 300, 40));
+			subTotalText.BorderStyle = UITextBorderStyle.RoundedRect;
+			Add(subTotalText);
+
+
+			var generosityLabel = new UILabel(new RectangleF(10, 100, 300, 40)) { Text = "Generosity" };
+			Add(generosityLabel);
+
+			//var generosityText = new UITextField(new CGRect(10, 150, 300, 40));
+			//generosityText.BorderStyle = UITextBorderStyle.RoundedRect;
+			//Add(generosityText);
+
+			var slider = new UISlider(new CGRect(10, 159, 300, 40));
+			slider.MinValue = 0;
+			slider.MaxValue = 100;
+			Add(slider);
+
+			var labelTip = new UILabel(new RectangleF(10, 200, 300, 40)) { Text = "Tip" };
+			Add(labelTip);
+
+			var tipText = new UILabel(new RectangleF(10, 240, 300, 40));
+			Add(tipText);
+
+			var tipLabel = new UILabel(new RectangleF(10, 280, 300, 40)) { Text = "Total" };
+			Add(tipLabel);
+
+
+			var labelTotal = new UILabel(new RectangleF(10, 320, 300, 40));
+			Add(labelTotal);
+
+			var set = this.CreateBindingSet<FirstView, FirstViewModel>();
+			set.Bind(subTotalText).To(vm => vm.SubTotal);
+			set.Bind(slider).To(vm => vm.Generosity);
+			set.Bind(tipText).To(vm => vm.Tip);
+			set.Bind(labelTotal).To(vm => vm.Total);
+
+			set.Apply();
+
+
+
+
+
+
 
             //var set = this.CreateBindingSet<FirstView, N1TipCalc.ViewModels.FirstViewModel>();
             //set.Bind(label1).To(vm => vm.Hello);
