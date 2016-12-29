513294592210d0c55caedac33fb67b678192fa90
NeuUser <divikiran.ravela@neudesic.com>
2016-12-29 14:25:56 +0530

    Added default running view
    Added default running view

--- MvvmCross/NProjects/N1TipCalc/TicCalc.Touch/Views/FirstView.cs
+++ MvvmCross/NProjects/N1TipCalc/TicCalc.Touch/Views/FirstView.cs
@@ -1,5 +1,8 @@
+using System.Drawing;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.iOS.Views;
+using ObjCRuntime;
+using UIKit;
 
 namespace TicCalc.Touch.Views
 {
@@ -11,12 +14,21 @@ namespace TicCalc.Touch.Views
 
         public override void ViewDidLoad()
         {
+
+			View = new UIView() { BackgroundColor = UIColor.Red };
             base.ViewDidLoad();
 
-            var set = this.CreateBindingSet<FirstView, N1TipCalc.ViewModels.FirstViewModel>();
-            set.Bind(Label).To(vm => vm.Hello);
-            set.Bind(TextField).To(vm => vm.Hello);
-            set.Apply();
+			// ios7 layout
+			if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+				EdgesForExtendedLayout = UIRectEdge.None;
+
+			var label1 = new UILabel(new RectangleF(0, 0, 300, 40)) { Text = "Test", BackgroundColor = UIColor.Green };
+			Add(label1);
+
+            //var set = this.CreateBindingSet<FirstView, N1TipCalc.ViewModels.FirstViewModel>();
+            //set.Bind(label1).To(vm => vm.Hello);
+            ////set.Bind(TextField).To(vm => vm.Hello);
+            //set.Apply();
         }
     }
 }
---
+++ MvvmCross/NProjects/N1TipCalc/TicCalc.Touch/obj/Xcode/3/FirstView.h
@@ -0,0 +1,22 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <MvvmCross/MvvmCross.h>
+#import <UIKit/UIKit.h>
+
+
+@interface FirstView : UIViewController {
+	UILabel *_Label;
+	UITextField *_TextField;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *Label;
+
+@property (nonatomic, retain) IBOutlet UITextField *TextField;
+
+@end
